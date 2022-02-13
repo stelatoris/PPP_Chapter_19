ef959d1b65193bfde900553cc92596b31e29d401
stelatoris <78478872+stelatoris@users.noreply.github.com>
2022-02-13 22:37:24 +0100

    Ex 01 WIP

--- PPP_Chapter_19/PPP_Chapter_19.cpp
+++ PPP_Chapter_19/PPP_Chapter_19.cpp
@@ -4,162 +4,62 @@
 //	************************************************************************************
 //	* Programming Principles and Practice Using C++, Second Edition, Bjarne Stroustrup *
 //	************************************************************************************
-//  Drill
-//  1. Define template<typename T> struct S { T val; }; .
-//  2. Add a constructor, so that you can initialize with a T.
-//  3. Define variables of types S<int>, S<char>, S<double>, S<string>, and
-//     S<vector<int>>; initialize them with values of your choice.
-//  4. Read those values and print them.
-//  5. Add a function template get() that returns a reference to val.
-//  6. Put the definition of get() outside the class.
-//  7. Make val private.
-//  8. Do 4 again using get().
-//  9. Add a set() function template so that you can change val.
-//  10. Replace set() with an S<T>::operator=(const T&).Hint : Much simpler
-//  than §19.2.5.
-//  11. Provide constand non - const versions of get().
-//  12. Define a function template<typename T> read_val(T & v) that reads from
-//  cin into v.
-//  13. Use read_val() to read into each of the variables from 3 except the
-//  S<vector<int>> variable.
-//  14. Bonus: Define input and output operators(>> and << ) for vector<T>s.
-//  For both input and output use a{ val, val, val } format.That will allow
-//  read_val() to also handle the S<vector<int>> variable.
-//  Remember to test after each step.
+//	1. Write a template function f() that adds the elements of one vector<T> to
+//	the elements of another; for example, f(v1, v2) should do v1[i] += v2[i] for
+//	each element of v1.
 
 #include "std_lib_facilities.h"
 
-template<typename T> 
-struct S {
-    S() : val{ T() } {}
-    S(T v) : val{ v } {}        // Drill 2
-    T& get();                   // Drill 5
-    const T& get() const;       // Drill 11
-    void set(const T& v);
-    T& operator=(const T&);     // Drill 10
-private:                        // Drill 7
-    T val;
-};
-
-//Drill 6
-template<typename T> T& S<T>::get() { return val; }
-
-template<typename T> const T& S<T>::get() const { return val; }
-
-// Drill 9
-template<typename T> void S<T>::set(const T& v) { val = v; }
-
-// Drill 13
-template<typename T> T& S<T>::operator=(const T& t)
-{    
-    val = t;
-    return val;
+template <typename T>
+void f(vector<T>& v1, const vector<T>& v2)
+{
+	for (int i = 0; i < v2.size(); ++i) {
+		if (i > v1.size() - 1) v1.push_back(v2[i]);
+		else {
+			v1[i] += v2[i];
+		}
+	}
 }
 
-template<typename T> istream& operator>>(istream& is, S<T>& my_s)
+template <typename T>
+void print_vector(const vector<T>& v)
 {
-    T v;
-    is >> v;
-    if (!is) return is;
-    my_s = v;
-    return is;
-}
-
-  //14. Bonus: Define input and output operators(>> and << ) for vector<T>s.
-  //For both input and output use a{ val, val, val } format.That will allow
-  //read_val() to also handle the S<vector<int>> variable.
-  //Remember to test after each step.
- 
-template<typename T> istream& operator>>(istream& is, vector<T>& v)
-{
-    char ch1{};
-    char ch2{};
-    T val;
-    is >> ch1;
-    if (!is) return is;
-    if (ch1 != '{') {
-        is.clear(ios_base::failbit);
-        return is;
-    }
-    while (cin >> val >> ch2 && ch2==',') {        
-        v.push_back(val);
-    }
-    if (ch2 != '}') {
-        is.clear(ios_base::failbit);
-        return is;
-    }
-    v.push_back(val);
-    
-    return is;
-}
-
-template<typename T> ostream& operator<<(ostream& os, const vector<T>& v)
-{
-    os << "{ ";
-    for (int i = 0; i < v.size(); ++i) {
-        os << v[i];
-        if (i < v.size() - 1) os << ", ";
-    }
-    os << " }";
-    return os;
-}
-
-template<typename T> void read_val(T& v)
-{
-    cin >> v;
+	for (int i{}; i < v.size(); ++i) {
+		cout << v[i];
+		if (i < v.size() - 1) cout << ", ";
+		if (i == v.size() - 1) cout << '\n';
+	}
 }
 
 int main()
 {
-    // Drill 3
-    S<int> s_int{ 5 };
-    S<char> s_char{ 'x' };
-    S<double>s_dbl{ 3.14 };
-    S<string>s_string{ "Hello, world" };
-    S < vector<int>> s_vec{ vector < int>{1,2,3,4} };
+	// int
+	vector<int> v_int1{ 1,2,3,4,5,6,7,8,9 };
+	vector<int> v_int2{ 5,5,5,5,5 };
+	f(v_int1, v_int2);
+	print_vector(v_int1);
 
-    // Drill4
-    // Drill 8
-    /*cout <<"s_int: " << s_int.get() << '\n';
-    cout << "s_char: " << s_char.get() << '\n';
-    cout << "s_dbl: " << s_dbl.get() << '\n';
-    cout << "s_string: " << s_string.get() << '\n';
-    cout << "vector s_vec: ";
-    for (int i = 0; i < s_vec.get().size(); ++i) {
-        cout << s_vec.get()[i];
-        if (i < s_vec.get().size() - 1) cout << ',';
-    }*/
+	// double
+	vector<double> v_double1{ 1.1,2.2,3.3,4.4,5.5 };
+	vector<double> v_double2{ 5.5,5.5,5.5,5.5,5.5,5.5,5.5,5.5,5.5,5.5,5.5 };
+	f(v_double1, v_double2);
+	print_vector(v_double1);
 
-    /*
-    S<double>s_dbl2= s_dbl;
-    cout << "\ns_dbl2: " << s_dbl2.get() << '\n';
+	// char
+	vector<char> v_char1{ 'a','b','c','d','e'};
+	vector<char> v_char2{ 'a','b','c','d','e'};
+	f(v_char1, v_char2);
+	print_vector(v_char1);
 
-    // Drill 13
-    cout << "Input an int: ";
-    read_val(s_int);
-    cout << "s_int() read: " << s_int.get() << '\n';
+	// string
+	vector<string> v_string1{ "Mr. ", "Miss. ", "Dr. " };
+	vector<string> v_string2{ "Anderson", "Perry", "Strange" };
+	f(v_string1, v_string2);
+	print_vector(v_string1);
 
-    cout << "Input a char: ";
-    read_val(s_char);
-    cout << "s_char() read: " << s_char.get() << '\n';
-
-    cout << "Input a double: ";
-    read_val(s_dbl);
-    cout << "s_dbl() read: " << s_dbl.get() << '\n';
-
-    cout << "Input a string: ";
-    read_val(s_string);
-    cout << "s_string() read: " << s_string.get() << '\n';
-    */
-
-   
-    cout <<'\n' << s_vec.get()<<'\n';
-
-    //Drill 14
-    S < vector<int>> s_vec2;
-    cout << "Please enter a vector in v,v,v,v etc format:" << '\n';
-    read_val(s_vec2);
-
-    cout << s_vec2.get() << '\n';
+	char ch1='a';
+	char ch2 = 'a';
+	cout << ch1<<',' << ch2 << '\n';
+	cout <<"ch1+ch2=" << ch1 + ch2 << '\n';
 }
 
