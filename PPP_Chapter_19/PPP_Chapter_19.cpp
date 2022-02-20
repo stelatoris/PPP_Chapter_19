a1d19d00dc1750e4fad5a9cf20ff96cb8eab95d1
stelatoris <78478872+stelatoris@users.noreply.github.com>
2022-02-20 22:20:24 +0100

    Ex 04 WIP

--- PPP_Chapter_19/PPP_Chapter_19.cpp
+++ PPP_Chapter_19/PPP_Chapter_19.cpp
@@ -4,90 +4,224 @@
 //	************************************************************************************
 //	* Programming Principles and Practice Using C++, Second Edition, Bjarne Stroustrup *
 //	************************************************************************************
-//	3. Write a template class Pair that can hold a pair of values of any type.
-//	Use this to implement a simple symbol table like the one we used in the
-//	calculator(§7.8).
+//	4. Modify class Link from §17.9.3 to be a template with the type of value
+//	as the template argument.Then redo exercise 13 from Chapter 17 with
+//	Link<God>.
 
 #include "std_lib_facilities.h"
 
-template<typename T, typename U>
-class Pair {
-	T v1;
-	U v2;
+template<typename T>
+class Link {
 public:
-	Pair(T _v1, U _v2): v1{_v1}, v2{_v2}{}
-	Pair() : v1{T()}, v2{U()} {}
-	T val1() const { return v1; }
-	U val2() const { return v2; }
+	Link(T n, T m, T v, T w, Link<T>* p = nullptr, Link<T>* s = nullptr)
+		: god{ n, m, v, w }, prev{ p }, succ{ s } { }
+
+	struct God {
+		God(T n, T m, T v, T w)
+			: name{ n }, mythology{ m }, vehicle{ v }, weapon{ w } { }
+
+		T name;
+		T mythology;
+		T vehicle;
+		T weapon;
+	};
+
+	God god;
+
+	Link<T>* insert(Link<T>* n); // insert n before this object
+	Link<T>* add(Link<T>* n); // insert n after this object
+	Link<T>* erase(); // remove this object from list
+	Link<T>* find(const T& s); // find s in list
+	const Link<T>* find(const T& s) const; // find s in const list (see §18.5.1)
+
+	Link<T>* advance(int n) const; // move n positions in list
+	Link<T>* add_ordered(Link<T>* n);	// order in lexicographical order
+
+	Link<T>* next() const { return succ; }
+	Link<T>* previous() const { return prev; }
+
+private:
+	Link<T>* prev;
+	Link<T>* succ;
 };
 
-template<typename T, typename U>
-class Symbol_Table {
-	vector< Pair<T,U>> table;
-public:
-	Symbol_Table() {}
-	int size() const { return table.size(); }
-	void add_symbol(const T& t, const U& u) { table.push_back({ t,u }); }
-	vector< Pair<T, U>> get_table() const { return table; }
-	U operator[](const T& v);
-	const U operator[](const T& v) const;
-};
-
-template<typename T, typename U>
-U Symbol_Table<T, U>::operator[](const T& v)
+template<typename T>
+Link<T>* Link<T>::insert(Link<T>* n) // insert n before this object; return n
 {
-	for (auto& a : table)
-		if (a.val1() == v)
-			return a.val2();
+	if (n == nullptr) return this;
+	if (this == nullptr) return n;
+	n->succ = this; // this object comes after n
+	if (prev) prev->succ = n;
+	n->prev = prev; // this object’s predecessor becomes n’s predecessor
+	prev = n; // n becomes this object’s predecessor
+	return n;
 }
 
-template<typename T, typename U>
-const U Symbol_Table<T, U>::operator[](const T& v) const
+template<typename T>
+Link<T>* Link<T>::add(Link<T>* n) // insert n after p; return n
 {
-	for (auto& a : table)
-		if (a.val1() == v)
-			return a.val2();
+	if (n == nullptr) return this;
+	if (this == nullptr) return n;
+	n->prev = this; // this object comes before n
+	if (succ) succ->prev = n;
+	n->succ = succ;	//this objects successor becomes n's successor
+	succ = n;
+	return n;
 }
 
-template<typename T, typename U>
-void print_vector(const Symbol_Table<T, U>& st)
-// for testing
+template<typename T>
+Link<T>* Link<T>::erase() // remove *p from list; return p’s successor
 {
-	for (int i = 0; i < st.size(); ++i) {
-		cout << st.get_table()[i].val1()
-			<<','
-			<< st.get_table()[i].val2()<< '\n';
+	if (this == nullptr) return nullptr;
+	if (succ) succ->prev = prev;
+	if (prev) prev->succ = succ;
+	return succ;
+}
+
+template<typename T>
+Link<T>* Link<T>::find(const T& s) // find s in list;
+// return nullptr for “not found”
+{
+	Link<T>* p = this;
+	while (p) {
+		if (p->god.name == s) return p;
+		p = p->succ;
+	}
+	return nullptr;
+}
+
+template<typename T>
+Link<T>* Link<T>::advance(int n) const // move n positions in list
+// return nullptr for “not found”
+// positive n moves forward, negative backward
+{
+	Link<T>* p = const_cast<Link<T>*>(this);
+	if (this == nullptr) return nullptr;
+	if (0 < n) {
+		while (n--) {
+			if (succ == nullptr) return nullptr;
+			p = p->succ;
+		}
+	}
+	else if (n < 0) {
+		while (n++) {
+			if (p->prev == nullptr) return nullptr;
+			p = p->prev;
+		}
+	}
+	return p;
+}
+
+template<typename T>
+Link<T>* Link<T>::add_ordered(Link<T>* n)
+{
+	Link<T>* p = this;
+
+	while (p) {
+
+		if (n->god.name < p->god.name) {			// if n is smaller than this(p)
+			if (p->prev == nullptr) {				// if this(p) is 1st link
+				p = p->insert(n);
+				return p;
+			}
+			else p = p->advance(-1);
+		}
+		else if (p->god.name < n->god.name) {		// if n is larger than this(p)
+			if (p->succ == nullptr) {
+				p = p->add(n);
+				return p;
+			}
+			else if (n->god.name < p->succ->god.name) {
+				p = p->add(n);
+				return p;
+			}
+			else p = p->advance(1);
+		}
 	}
 }
 
-void create_ascii_vector(Symbol_Table<char, int>& v)
+template<typename T>
+void lexicograph_order(Link<T>* in, Link<T>* out)
 {
-	for (int i = 32; i < 127; ++i) {
-		v.add_symbol(char(i), i);
+	while (in) {	// go back to 1st Link
+		if (!in->previous()) break;
+		in = in->previous();
+	}
+
+	string myth = out->god.mythology;	//which mythology to search for?
+
+	while (in) {
+		if (in->god.mythology == myth) {	// extract only gods from specified mythology
+			Link<T>* s = out->find(in->god.name);
+			if (s == nullptr) {
+				Link<T>* p = in;				// copy link to temp
+				in = in->erase();			// delete link from original list
+				out = out->add_ordered(p);	// add the link from temp
+			}
+			else if (s->god.name == in->god.name) {
+				cout << in->god.name << " already entered." << '\n';
+			}
+		}
+		else in = in->next();				// keep going through the list
+	}
+}
+
+template<typename T>
+void print_all(Link<T>* p)
+{
+	while (p) {
+		if (!p->previous()) break;
+		p = p->previous();
+	}
+
+	if (p->god.mythology == "Norse") cout << "Norse gods:\n";
+	else if (p->god.mythology == "Roman") cout << "Roman gods:\n";
+	else if (p->god.mythology == "Greek") cout << "Greek gods:\n";
+	else {}
+
+	while (p) {
+		cout << '-' << p->god.name << ',' << p->god.mythology
+			<< ',' << p->god.vehicle << ',' << p->god.weapon;
+		if (p = p->next()) cout << '\n';
 	}
 }
 
 int main()
 try {	
-	Symbol_Table<string,double> st;
-	st.add_symbol("PI", 3.14159'26535'89793'23846);
-	st.add_symbol("Phi", 1.61803'39887'49894'84820);
-	st.add_symbol("Omega", 0.56714'32904'09783'87299);
-	st.add_symbol("c", 299'792'458);	
+	Link<string>* gods = new Link<string>{ "Hades ","Greek","","The Helm of Darkness" };
+	gods = gods->add(new Link<string>{ "Poseidon ","Greek","The Chariot of the Sea","The Trident of Poseidon" });
+	gods = gods->add(new Link<string>{ "Saturn ","Roman","","Scythe" });
+	gods = gods->add(new Link<string>{ "Aphrodite","Greek","","Cestus " });
+	gods = gods->add(new Link<string>{ "Odin","Norse","Eight-legged flying horse, Sleipnir","Gungnir the spear" });
+	gods = gods->add(new Link<string>{ "Freya ","Norse","cats of Freya","Dainsleif (sword)" });
+	gods = gods->add(new Link<string>{ "Jupiter ","Roman","","Lightning Bolt" });
+	gods = gods->add(new Link<string>{ "Heimdall","Norse","","Gjallarhorn the Yelling Horn" });
+	gods = gods->add(new Link<string>{ "Vidar","Norse","","Vidar's Shoes" });
+	gods = gods->add(new Link<string>{ "Ullr ","Norse","","Arrow" });
+	gods = gods->add(new Link<string>{ "Athena","Greek","","The Aegis" });
+	gods = gods->add(new Link<string>{ "Juno","Roman","Cart","Spear" });
+	gods = gods->add(new Link<string>{ "Minerva","Roman","","Spear" });
+	gods = gods->add(new Link<string>{ "Mars","Roman","","Spear" });
 
-	//vector<Symbol_Table<int, string>> s_table{ {40, "John"},{41, "Smith"}, {43, "Arnold"}, {18, "Conner"} };
-	//print_vector(st);
 
-	cout <<"PI:"<< st["PI"] << '\n';
-	cout << "Phi: " << st["Phi"] << '\n';
-	cout << "c: " << st["c"] << '\n';
-	cout << "Omega: " << st["Omega"] << '\n';
+	//print_all(gods);
 
-	cout << "-------------------------------------------\n";
-	// Created an ASCII table for fun
-	Symbol_Table<char, int> ascii;
-	create_ascii_vector(ascii);
-	print_vector(ascii);
+	Link<string>* norse_gods = new Link<string>{ "Thor","Norse","Chariot of Thunder","Hammer" };
+	Link<string>* greek_gods = new Link<string>{ "Zeus ","Greek","","Thunderbolts" };
+	Link<string>* roman_gods = new Link<string>{ "Apollo","Roman","Sun Chariot","Silver Bow and Arrow" };
+
+	lexicograph_order(gods, greek_gods);
+	lexicograph_order(gods, norse_gods);
+	lexicograph_order(gods, roman_gods);
+
+	print_all(greek_gods);
+	cout << '\n';
+	print_all(norse_gods);
+	cout << '\n';
+	print_all(roman_gods);
+	cout << '\n';
+
+	return 0;
 }
 
 catch (exception& e) {
