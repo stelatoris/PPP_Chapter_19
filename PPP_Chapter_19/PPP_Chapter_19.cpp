a2c0c79d1afacedff72373bf3424842df39b3513
stelatoris <78478872+stelatoris@users.noreply.github.com>
2022-03-23 16:56:58 +0100

    Ex 08 done.

--- PPP_Chapter_19/PPP_Chapter_19.cpp
+++ PPP_Chapter_19/PPP_Chapter_19.cpp
@@ -16,16 +16,16 @@
 template<typename T>
 class allocator {
 public:
-	T* allocate(int n) { return malloc(n*sizeof(T)); }
+	T* allocate(int n) { return (T*) malloc(n*sizeof(T)); }
 	void deallocate(T* p) { free(p); }
 
-	void construct(T* p, const T& v) { new (p) T(v); }	// std::allocator::construct
+	void construct(T* p, const T& v) { new (p) T{ v }; }	// std::allocator::construct
 	void destroy(T* p) { p->~T(); }						// std::allocator::destroy
 };
 
 
 // an almost real vector of Ts:
-template<typename T, typename A=allocator<T>>
+template<typename T, typename A = allocator<T>>
 class vector { // read “for all types T” (just like in math)
 	A alloc;	// use allocate to handle memory for elements
 	int sz; // the size
@@ -37,17 +37,26 @@ public:
 	{
 		for (int i = 0; i < sz; ++i) elem[i] = 0; // elements are initialized
 	}
-	vector(const vector&); // copy constructor
+	vector(const vector& a)                 // copy constructor
+        : sz{a.sz}, elem{new T[a.sz]}, space{a.sz}
+    {
+        for (int i = 0; i < a.sz; ++i)
+            elem[i] = a.elem[i];
+    }
+
 	vector& operator=(const vector&); // copy assignment
 	vector(vector&&); // move constructor
 	vector& operator=(vector&&); // move assignment
 	~vector() { delete[] elem; } // destructor
+	T& at(int n);
+	const T& at(int n) const;
+
 	T& operator[] (int n) { return elem[n]; } // access: return reference
 	const T& operator[] (int n) const { return elem[n]; }
 	int size() const { return sz; } // the current size
 	int capacity() const { return space; }
-	void resize(int newsize); // growth
-	void push_back(const T& d);
+	void resize(int newsize, T val = T()); // growth
+	void push_back(const T& val);
 	void reserve(int newalloc);
 };
 
@@ -90,34 +99,43 @@ vector<T,A>& vector<T,A>::operator=(vector&& a) // move a to this vector
 template<typename T, typename A>
 void vector<T,A>::reserve(int newalloc)
 {
-	if (newalloc <= space) return; // never decrease allocation
-	T* p = new T[newalloc]; // allocate new space
+	if (newalloc <= space) return;
+	T* p = alloc.allocate(newalloc);
+	
 	for (int i = 0; i < sz; ++i) p[i] = elem[i]; // copy old elements
-	delete[] elem; // deallocate old space
+	alloc.deallocate(elem);
 	elem = p;
 	space = newalloc;
 }
 
 template<typename T, typename A>
-void vector<T,A>::resize(int newsize)
+void vector<T,A>::resize(int newsize, T val)
 // make the vector have newsize elements
 // initialize each new element with the default value 0.0
 {
 	reserve(newsize);
-	for (int i = sz; i < newsize; ++i) elem[i] = 0; // initialize new elements
+	for (int i = sz; i < newsize; ++i) alloc.construct(&elem[i], val);
 	sz = newsize;
 }
 
 template<typename T, typename A>
-void vector<T,A>::push_back(const T& d)
+void vector<T,A>::push_back(const T& val)
 // increase vector size by one; initialize the new element with d
 {
 	if (space == 0)
 		reserve(8); // start with space for 8 elements
 	else if (sz == space)
 		reserve(2 * space); // get more space
-	elem[sz] = d; // add d at end
-	++sz; // increase the size (sz is the number of elements)
+	alloc.construct(&elem[sz], val);
+	++sz;
+}
+
+template<typename T, typename A>
+T& vector<T, A>::at(int n)
+{
+	if (n < 0 || sz <= n) 
+		throw std::range_error("out of range vector access");
+	return elem[n];
 }
 
 template<typename T>
@@ -142,13 +160,10 @@ try {
 	vector<int>v2;
 	v2 = v_int1;
 	v2.push_back(6);
-	//vector<int>v3{ v2 };
+	vector<int>v3{ v2 };
 
 	print_vector(v_int1);
 	print_vector(v2);
-
-	
-
 }
 
 catch (std::exception& e) {
